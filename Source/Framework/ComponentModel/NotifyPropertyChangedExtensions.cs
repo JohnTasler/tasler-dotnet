5345cb1a4a9b55413071f1a248d5e6334367df74
John Tasler <JohnTasler@users.noreply.github.com>
2017-10-21 02:11:14 -0700

    Create NotifyPropertyChangedExtensions.cs
    Adds very useful extension methods on any INotifyPropertyChanged implementation.

---
+++ Source/Framework/ComponentModel/NotifyPropertyChangedExtensions.cs
@@ -0,0 +1,55 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Tasler.ComponentModel
+{
+    public static class NotifyPropertyChangedExtensions
+    {
+        public static bool SetProperty<TSender, TValue>(this PropertyChangedEventHandler @this, TSender sender, ref TValue field, TValue newValue, [CallerMemberName] string propertyName = null)
+            where TSender : INotifyPropertyChanged
+        {
+            var propertyChanged = !object.Equals(field, newValue);
+            if (propertyChanged)
+            {
+                field = newValue;
+                @this.RaisePropertyChanged(sender, propertyName);
+            }
+            return propertyChanged;
+        }
+
+        public static bool SetProperty<TSender, TValue>(this PropertyChangedEventHandler @this, TSender sender, ref TValue field, TValue newValue, params string[] propertyNames)
+            where TSender : INotifyPropertyChanged
+        {
+            var propertyChanged = !object.Equals(field, newValue);
+            if (propertyChanged)
+            {
+                field = newValue;
+                @this.RaisePropertyChanged(sender, propertyNames);
+            }
+            return propertyChanged;
+        }
+
+        public static void RaisePropertyChanged<T>(this PropertyChangedEventHandler @this, T sender, string propertyName)
+            where T : INotifyPropertyChanged
+        {
+            if (@this != null && propertyName != null)
+            {
+                @this.Invoke(sender, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public static void RaisePropertyChanged<T>(this PropertyChangedEventHandler @this, T sender, params string[] propertyNames)
+            where T : INotifyPropertyChanged
+        {
+            if (@this != null && propertyNames != null)
+            {
+                var length = propertyNames.Length;
+                for (var index = 0; index < length; ++index)
+                {
+                    var propertyName = propertyNames[index];
+                    @this.Invoke(sender, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+        }
+    }
+}
