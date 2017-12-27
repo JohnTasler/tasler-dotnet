5c619f47bf5818bfa5d32823a94187dde1690af4
John Tasler <taz@mysticpop.com>
2017-12-26 23:25:48 -0800

    chore: Modernize Tasler.ComponentModel framework, including move to GitHub

---
+++ Source/Tasler.ComponentModel/IMultipleSelectionContainer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Tasler.ComponentModel
+{
+	public interface IMulitpleSelectionContainer
+	{
+		IEnumerable<object> SelectedItems { get; }
+
+		void AddToSelectedItems(object item);
+		void RemoveFromSelectedItems(object item);
+	}
+}
---
+++ Source/Tasler.ComponentModel/IParentedObject.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace Tasler.ComponentModel
+{
+	public interface IParentedObject
+	{
+		object GetParent();
+
+		bool SetParent(object parent);
+	}
+
+	public interface IParentedObject<TParent> : IParentedObject
+		where TParent : class, INotifyPropertyChanged
+	{
+		TParent Parent { get; }
+
+		bool SetParent(TParent parent);
+	}
+}
