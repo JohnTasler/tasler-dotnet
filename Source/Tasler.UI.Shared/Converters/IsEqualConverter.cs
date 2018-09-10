d9d9ad04887bb4fc6914289eef1423dc43192bc2
John Tasler <taz@mysticpop.com>
2018-09-09 20:22:59 -0700

    chore: Move IsEqualConverter to Tasler.NET

---
+++ Source/Tasler.UI.Shared/Converters/IsEqualConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+#if WINDOWS_UWP
+using ConverterBase = Tasler.UI.Xaml.Converters.BaseValueConverter;
+using CultureInfo = System.String;
+namespace Tasler.UI.Xaml.Converters
+#elif WINDOWS_WPF
+using System.Globalization;
+using ConverterBase = Tasler.Windows.Converters.SingletonValueConverter<Tasler.Windows.Converters.IsEqualConverter>;
+namespace Tasler.Windows.Converters
+#endif
+{
+    public class IsEqualConverter : ConverterBase
+    {
+        #region IValueConverter Members
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return parameter == null;
+
+            var valueType = value.GetType();
+            if (parameter != null && parameter?.GetType() != valueType)
+                parameter = System.Convert.ChangeType(parameter, valueType, GetCultureInfo(culture));
+
+            return object.Equals(value, parameter);
+        }
+
+        #endregion IValueConverter Members
+    }
+}
