b30c7256c7246393f2c64671a903f94a7203eeb2
John Tasler <taz@mysticpop.com>
2018-12-23 08:42:16 -0800

    Change TextWriterExentsion methods use a typed TextWriter argument

--- Source/Tasler.Core/IO/Extensions/TextWriterExtensions.cs
+++ Source/Tasler.Core/IO/Extensions/TextWriterExtensions.cs
@@ -7,217 +7,253 @@ namespace Tasler.IO
     {
         public static void Write(this TextWriter @this, ReadOnlySpan<char> span) => @this.Write(span.ToArray());
 
-        public static TextWriter Append(this TextWriter @this, ReadOnlySpan<char> span)
+        public static T Append<T>(this T @this, ReadOnlySpan<char> span)
+            where T : TextWriter
         {
             @this.Write(span.ToArray());
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, ulong value)
+        public static T Append<T>(this T @this, ulong value)
+            where T : TextWriter
         {
             @this.Write(value);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, uint value)
+        public static T Append<T>(this T @this, uint value)
+            where T : TextWriter
         {
             @this.Write(value);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, string format, params object[] arg)
+        public static T Append<T>(this T @this, string format, params object[] arg)
+            where T : TextWriter
         {
             @this.Write(format, arg);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, string format, object arg0, object arg1, object arg2)
+        public static T Append<T>(this T @this, string format, object arg0, object arg1, object arg2)
+            where T : TextWriter
         {
             @this.Write(format, arg0, arg1, arg2);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, string format, object arg0, object arg1)
+        public static T Append<T>(this T @this, string format, object arg0, object arg1)
+            where T : TextWriter
         {
             @this.Write(format, arg0, arg1);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, string format, object arg0)
+        public static T Append<T>(this T @this, string format, object arg0)
+            where T : TextWriter
         {
             @this.Write(format, arg0);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, string value)
+        public static T Append<T>(this T @this, string value)
+            where T : TextWriter
         {
             @this.Write(value);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, object value)
+        public static T Append<T>(this T @this, object value)
+            where T : TextWriter
         {
             @this.Write(value);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, long value)
+        public static T Append<T>(this T @this, long value)
+            where T : TextWriter
         {
             @this.Write(value);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, int value)
+        public static T Append<T>(this T @this, int value)
+            where T : TextWriter
         {
             @this.Write(value);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, double value)
+        public static T Append<T>(this T @this, double value)
+            where T : TextWriter
         {
             @this.Write(value);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, decimal value)
+        public static T Append<T>(this T @this, decimal value)
+            where T : TextWriter
         {
             @this.Write(value);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, char[] buffer, int index, int count)
+        public static T Append<T>(this T @this, char[] buffer, int index, int count)
+            where T : TextWriter
         {
             @this.Write(buffer, index, count);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, char[] buffer)
+        public static T Append<T>(this T @this, char[] buffer)
+            where T : TextWriter
         {
             @this.Write(buffer);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, char value)
+        public static T Append<T>(this T @this, char value)
+            where T : TextWriter
         {
             @this.Write(value);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, bool value)
+        public static T Append<T>(this T @this, bool value)
+            where T : TextWriter
         {
             @this.Write(value);
             return @this;
         }
 
-        public static TextWriter Append(this TextWriter @this, float value)
+        public static T Append<T>(this T @this, float value)
+            where T : TextWriter
         {
             @this.Write(value);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, string format, object arg0)
+        public static T AppendLine<T>(this T @this, string format, object arg0)
+            where T : TextWriter
         {
             @this.WriteLine(format, arg0);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, ulong value)
+        public static T AppendLine<T>(this T @this, ulong value)
+            where T : TextWriter
         {
             @this.WriteLine(value);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, uint value)
+        public static T AppendLine<T>(this T @this, uint value)
+            where T : TextWriter
         {
             @this.WriteLine(value);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, string format, params object[] arg)
+        public static T AppendLine<T>(this T @this, string format, params object[] arg)
+            where T : TextWriter
         {
             @this.WriteLine(format, arg);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, string format, object arg0, object arg1, object arg2)
+        public static T AppendLine<T>(this T @this, string format, object arg0, object arg1, object arg2)
+            where T : TextWriter
         {
             @this.WriteLine(format, arg0, arg1, arg2);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, string format, object arg0, object arg1)
+        public static T AppendLine<T>(this T @this, string format, object arg0, object arg1)
+            where T : TextWriter
         {
             @this.WriteLine(format, arg0, arg1);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, string value)
+        public static T AppendLine<T>(this T @this, string value)
+            where T : TextWriter
         {
             @this.WriteLine(value);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, float value)
+        public static T AppendLine<T>(this T @this, float value)
+            where T : TextWriter
         {
             @this.WriteLine(value);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this)
+        public static T AppendLine<T>(this T @this)
+            where T : TextWriter
         {
             @this.WriteLine();
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, long value)
+        public static T AppendLine<T>(this T @this, long value)
+            where T : TextWriter
         {
             @this.WriteLine(value);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, int value)
+        public static T AppendLine<T>(this T @this, int value)
+            where T : TextWriter
         {
             @this.WriteLine(value);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, double value)
+        public static T AppendLine<T>(this T @this, double value)
+            where T : TextWriter
         {
             @this.WriteLine(value);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, decimal value)
+        public static T AppendLine<T>(this T @this, decimal value)
+            where T : TextWriter
         {
             @this.WriteLine(value);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, char[] buffer, int index, int count)
+        public static T AppendLine<T>(this T @this, char[] buffer, int index, int count)
+            where T : TextWriter
         {
             @this.WriteLine(buffer, index, count);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, char[] buffer)
+        public static T AppendLine<T>(this T @this, char[] buffer)
+            where T : TextWriter
         {
             @this.WriteLine(buffer);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, char value)
+        public static T AppendLine<T>(this T @this, char value)
+            where T : TextWriter
         {
             @this.WriteLine(value);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, bool value)
+        public static T AppendLine<T>(this T @this, bool value)
+            where T : TextWriter
         {
             @this.WriteLine(value);
             return @this;
         }
 
-        public static TextWriter AppendLine(this TextWriter @this, object value)
+        public static T AppendLine<T>(this T @this, object value)
+            where T : TextWriter
         {
             @this.WriteLine(value);
             return @this;
