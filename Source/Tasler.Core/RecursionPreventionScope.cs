7d786de20e1187b494c5715c1f83435b1d8d78e0
John Tasler <taz@mysticpop.com>
2025-09-20 18:09:25 -0700

    chore: update dep vers; add class doc

--- Source/Tasler.Core/RecursionPreventionScope.cs
+++ Source/Tasler.Core/RecursionPreventionScope.cs
@@ -1,6 +1,21 @@
 
 namespace Tasler;
 
+/// <summary>
+/// Encapsulates the pattern of preventing a recursive call into a method by setting a
+/// <see langword="bool"/> to true for the remainder of the scope.
+/// </summary>
+/// <example>
+///   partial void OnTextChanged(string oldValue, string newValue)
+///   {
+///     using var textChangeScope = RecursionPreventionScope.GetScope(ref _isInTextChange);
+///     if (textChangeScope.IsInScope)
+///       return;
+///
+///     this.Text = "something else"; // Because of the recursion prevention the processing after
+///                                   // the <see langword="if"/> statement above will not occur again.
+/// </example>
+/// <seealso cref="System.IDisposable" />
 public readonly ref struct RecursionPreventionScope : IDisposable
 {
 	private readonly ref bool _flag;
