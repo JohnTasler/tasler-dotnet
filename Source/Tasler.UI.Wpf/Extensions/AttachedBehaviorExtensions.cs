588ecbfaf4f38031de9db60917981558d60b994e
John Tasler <taz@mysticpop.com>
2026-02-11 01:56:01 -0800

    Fixed duplicate behavior issue

--- Source/Tasler.UI.Wpf/Extensions/AttachedBehaviorExtensions.cs
+++ Source/Tasler.UI.Wpf/Extensions/AttachedBehaviorExtensions.cs
@@ -1,4 +1,3 @@
-
 using System.Windows;
 using CommunityToolkit.Diagnostics;
 using Microsoft.Xaml.Behaviors;
@@ -7,13 +6,13 @@ namespace Tasler.Windows.Extensions;
 
 public static class AttachedBehaviorExtensions
 {
-	public static void BehaviorPropertyChanged<TElement, TBehacvior, TValue>(
+	public static void BehaviorPropertyChanged<TElement, TBehavior, TValue>(
 		this DependencyPropertyKey dpk,
 		DependencyObject d,
 		DependencyPropertyChangedEventArgs e,
 		Func<TValue, bool>? isDefaultTest)
 		where TElement : FrameworkElement
-		where TBehacvior : Behavior<TElement>, new()
+		where TBehavior : Behavior<TElement>, new()
 	{
 		Guard.IsNotNull(dpk);
 		Guard.IsNotNull(d);
@@ -24,7 +23,7 @@ public static class AttachedBehaviorExtensions
 		var newValue = (TValue)e.NewValue;
 		if (isDefaultTest is not null && isDefaultTest(newValue))
 		{
-			var behavior = (TBehacvior)element.GetValue(dpk.DependencyProperty);
+			var behavior = (TBehavior)element.GetValue(dpk.DependencyProperty);
 			if (behavior is not null)
 			{
 				// Detach and dereference the behavior when the newValue is its default value
@@ -34,25 +33,27 @@ public static class AttachedBehaviorExtensions
 		}
 		else
 		{
-			// Detach any existing behavior before creating a new one
-			var existing = (TBehacvior)element.GetValue(dpk.DependencyProperty);
-			existing?.Detach();
-
-			// Create and attach the behavior only when the newValue is not its default value
-			var behavior = new TBehacvior();
-			behavior.Attach(element);
-			element.SetValue(dpk, behavior);
+			// Create and attach the behavior or reuse the existing behavior
+			if (element.GetValue(dpk.DependencyProperty) is TBehavior existing)
+			{
+				var behavior = existing is null ? new TBehavior() : existing;
+				if (behavior != existing)
+				{
+					behavior.Attach(element);
+					element.SetValue(dpk, behavior);
+				}
+			}
 		}
 	}
 
-	public static void BehaviorPropertyChanged<TElement, TBehacvior, TValue>(
+	public static void BehaviorPropertyChanged<TElement, TBehavior, TValue>(
 		this DependencyPropertyKey dpk,
 		DependencyObject d,
 		DependencyPropertyChangedEventArgs e,
 		TValue defaultValue = default(TValue)!)
 		where TElement : FrameworkElement
-		where TBehacvior : Behavior<TElement>, new()
+		where TBehavior : Behavior<TElement>, new()
 	{
-		dpk.BehaviorPropertyChanged<TElement, TBehacvior, TValue>(d, e, v => e.NewValue.Equals((object)defaultValue!));
+		dpk.BehaviorPropertyChanged<TElement, TBehavior, TValue>(d, e, v => e.NewValue.Equals((object)defaultValue!));
 	}
 }
