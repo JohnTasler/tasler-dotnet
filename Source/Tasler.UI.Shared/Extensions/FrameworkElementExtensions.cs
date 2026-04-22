b54f65756fbc8e3fdca357d23c918b898b8e3af3
John Tasler <taz@MysticPop.com>
2026-04-22 00:14:12 -0700

    update FrameworkElementExtensions

--- Source/Tasler.UI.Shared/Extensions/FrameworkElementExtensions.cs
+++ Source/Tasler.UI.Shared/Extensions/FrameworkElementExtensions.cs
@@ -6,12 +6,15 @@ namespace Tasler.Windows;
 
 public static class FrameworkElementExtensions
 {
-	public static void HookDataContextAsViewModel<T>(this T @this, PropertyChangedEventHandler? handler)
+	public static void HookDataContextAsViewModel<T, TViewModel>(this T @this, TViewModel viewModel, PropertyChangedEventHandler? handler)
 		where T : FrameworkElement, INotifyPropertyChanged
+		where TViewModel : INotifyPropertyChanged
 	{
 		DependencyPropertyDescriptor
 			.FromProperty(FrameworkElement.DataContextProperty, typeof(T))
 			.AddValueChanged(@this, Element_DataContextChanged);
+
+		@this.DataContext = viewModel;
 
 		void Element_DataContextChanged(object? sender, EventArgs e)
 		{
